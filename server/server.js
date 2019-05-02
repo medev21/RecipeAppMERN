bea01cb62845304972f07fbcac0212c49a565282
medev21 <benmarteen07@gmail.com>
2019-05-02 13:45:14 -0400

    set up basic server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// import libraries
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+const router = express.Router();
+
+// initialize app
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+router.get("/", (request, response) => {
+    return response.status(200).json({message: 'hello world'});
+});
+
+module.exports = app;
