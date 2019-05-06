84d41cc9ed8f4458e5e420116008e57fac44ee88
medev21 <benmarteen07@gmail.com>
2019-05-05 21:28:40 -0400

    set up mongoose connection

--- server/server.js
+++ server/server.js
@@ -8,7 +8,19 @@ const app = express();
 // local imports
 const RecipeRoute = require('./routes/recipes');
 
-// initialize app
+// DB connection setup
+require('dotenv').config();
+const DBUSER = process.env.DBUSER;
+const DBPASSWORD = process.env.DBPASSWORD;
+const URI = `mongodb://${DBUSER}:${DBPASSWORD}@ds151076.mlab.com:51076/recipedb`;
+mongoose.connect(URI, {useNewUrlParser: true});
+const conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connectionERROR:'));
+conn.once('open', () => {
+    console.log("connected to database");
+});
+
+// set up morgon for log requests and body parser for requests
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
